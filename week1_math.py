f52dbc87433b15f3a863f6791f492167377ab5c5
Robert Layton <robertlayton@gmail.com>
2017-11-15 11:52:10 +1100

    Week 1 code math sample

---
+++ week1_math.py
@@ -0,0 +1,15 @@
+def add(a, b):
+    return a + b
+ 
+def multiply(a, b):
+    sum = 0
+    for i in range(b):
+        sum += a
+    return sum # you could do "a * b"
+ 
+print("3 + 4 =", add(3, 4))
+print("3 * 4 =", multiply(3, 4))
+
+mylist = [1, 2, 3]
+
+
